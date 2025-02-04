159044bd2a7a6b2c04115f36fc9a7ddf552dfdd0
daviddprtma <davidchristianpratama@gmail.com>
2025-02-04 11:08:05 +0700

    update done

--- frontend/src/components/ui/commandbox.tsx
+++ frontend/src/components/ui/commandbox.tsx
@@ -9,9 +9,8 @@ const CommandBox: React.FC<CommandBoxProps> = ({ onReset }) => {
   return (
     <div className="fixed bottom-4 left-4 bg-[#18181A] p-2 border border-gray-600 rounded-lg shadow-lg flex items-center space-x-2 z-50">
       <div className="flex items-center bg-[#27272A] text-gray-400 px-2 py-1 rounded-md">
-        <span className="text-xs">⌘</span>
-        <span className="text-xs mx-1">+</span>
-        <span className="text-xs">K</span>
+        
+        <span className="text-xs">Reload</span>
       </div>
       <Button
         onClick={onReset}
--- frontend/src/components/ui/commandboxrun.tsx
+++ frontend/src/components/ui/commandboxrun.tsx
@@ -25,9 +25,7 @@ const CommandBoxRun: React.FC<CommandBoxRunProps> = ({
   return (
     <div className="fixed bottom-4 right-4 bg-[#18181A] p-2 border border-gray-600 rounded-lg shadow-lg flex items-center space-x-2 z-50">
       <div className="flex items-center bg-[#27272A] text-gray-400 px-2 py-1 rounded-md">
-        <span className="text-xs">⌘</span>
-        <span className="text-xs mx-1">+</span>
-        <span className="text-xs">J</span>
+        <span className="text-xs">Run</span>
       </div>
       <Button
         onClick={handleRun}
